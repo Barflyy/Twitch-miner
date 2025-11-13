46add3f8ec4f9598e7710dc2cfd5eadca6f81d67
Barflyy <56941919+Barflyy@users.noreply.github.com>
2025-11-13 18:59:59 +0100

    Create run.py

---
+++ run.py
@@ -0,0 +1,28 @@
+import logging
+from TwitchChannelPointsMiner import TwitchChannelPointsMiner
+from TwitchChannelPointsMiner.logger import LoggerSettings, ColorPalette
+from TwitchChannelPointsMiner.classes.entities.Bet import Strategy, BetSettings, Condition, OutcomeKeys, FilterCondition, DelayMode
+from TwitchChannelPointsMiner.classes.entities.Streamer import Streamer, StreamerSettings
+
+twitch_miner = TwitchChannelPointsMiner(
+    username="VOTRE_USERNAME_TWITCH",
+    claim_drops_startup=False,
+    priority=[
+        # Liste des streamers à miner
+        Streamer("streamer1"),
+        Streamer("streamer2"),
+    ],
+    logger_settings=LoggerSettings(
+        save=True,
+        console_level=logging.INFO,
+        file_level=logging.DEBUG,
+        emoji=True,
+        colored=True,
+        color_palette=ColorPalette(
+            STREAMER_online="green",
+            streamer_offline="red"
+        )
+    )
+)
+
+twitch_miner.mine()
